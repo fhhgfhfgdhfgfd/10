6674212bcbc6117c0bc3a5d7f3e5800076894814
markoza546556 <iraklidalaqisvili@gmail.com>
2025-04-24 07:40:52 -0700

    im

---
+++ Assets/სკრიპტ/002.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class c002b : MonoBehaviour
+{
+    public float moveSpeed = 5f; // სიჩქარე წინსვლისთვის
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // წინ წასვლა W ღილაკზე
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+    }
+}
